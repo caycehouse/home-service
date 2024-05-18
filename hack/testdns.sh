c0d673a793ad7448d1dd5a4ed5d8177c0c75fada
Cayce House <cayce@caycehouse.com>
2024-05-18 15:13:28 -0400

    minor changes

--- hack/testdns.sh
+++ hack/testdns.sh
@@ -2,8 +2,6 @@
 
 echo "BLOCKY external query"
 dig +short @192.168.4.121 google.com | sed 's/^/  /'
-echo "BIND external query"
-dig +short @192.168.4.123 google.com | sed 's/^/  /'
 echo "---"
 echo "BLOCKY internal query"
 dig +short @192.168.4.121 unifi.internal | sed 's/^/  /'
@@ -17,4 +15,4 @@ dig +short @192.168.4.123 echo-server.caycehouse.com | sed 's/^/  /'
 echo "---"
 echo "BLOCKY reverse query"
 dig +short @192.168.4.121 -x 192.168.7.28 | sed 's/^/  /'
-echo "---"
+echo "---------------------------------------------------"
