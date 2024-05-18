3560b7f734afaeb0dd78bf209a344d954b4ae083
Cayce House <cayce@caycehouse.com>
2024-05-18 15:46:36 -0400

    adjust domain names

--- hack/testdns.sh
+++ hack/testdns.sh
@@ -6,12 +6,12 @@ echo "---"
 echo "BLOCKY internal query"
 dig +short @192.168.4.121 unifi.internal | sed 's/^/  /'
 echo "BLOCKY internal BIND query"
-dig +short @192.168.4.121 unifi.house.casa | sed 's/^/  /'
+dig +short @192.168.4.121 unifi.housefam.win | sed 's/^/  /'
 echo "---"
 echo "BLOCKY main cluster query"
-dig +short @192.168.4.121 echo-server.caycehouse.com | sed 's/^/  /'
+dig +short @192.168.4.121 echo-server.housefam.casa | sed 's/^/  /'
 echo "BIND main cluster query"
-dig +short @192.168.4.123 echo-server.caycehouse.com | sed 's/^/  /'
+dig +short @192.168.4.123 echo-server.housefam.casa | sed 's/^/  /'
 echo "---"
 echo "BLOCKY reverse query"
 dig +short @192.168.4.121 -x 192.168.7.28 | sed 's/^/  /'
