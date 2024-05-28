89a1c839456c7dcc8ce613fe093e70138a14534e
Cayce House <cayce@caycehouse.com>
2024-05-28 12:53:05 -0400

    cleanup testdns.sh

--- hack/testdns.sh
+++ hack/testdns.sh
@@ -3,15 +3,13 @@
 echo "BLOCKY external query"
 dig +short @192.168.4.121 google.com | sed 's/^/  /'
 echo "---"
-echo "BLOCKY internal query"
+echo "BLOCKY internal UNIFI device query"
 dig +short @192.168.4.121 unifi.internal | sed 's/^/  /'
-echo "BLOCKY internal BIND query"
+echo "BLOCKY internal UNIFI custom query"
 dig +short @192.168.4.121 unifi.housefam.win | sed 's/^/  /'
 echo "---"
 echo "BLOCKY main cluster query"
 dig +short @192.168.4.121 echo-server.housefam.casa | sed 's/^/  /'
-echo "BIND main cluster query"
-dig +short @192.168.4.123 echo-server.housefam.casa | sed 's/^/  /'
 echo "---"
 echo "BLOCKY reverse query"
 dig +short @192.168.4.121 -x 192.168.7.28 | sed 's/^/  /'
