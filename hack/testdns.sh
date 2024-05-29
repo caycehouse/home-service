9bd4260c4e515b175a63da59ddea346665744439
Cayce House <cayce@caycehouse.com>
2024-05-29 10:19:55 -0400

    purge bind/blocky

--- hack/testdns.sh
+++ hack/testdns.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-echo "BLOCKY external query"
-dig +short @192.168.4.121 google.com | sed 's/^/  /'
+echo "UniFi external query"
+dig +short @192.168.4.1 google.com | sed 's/^/  /'
 echo "---"
-echo "BLOCKY internal UNIFI device query"
-dig +short @192.168.4.121 unifi.internal | sed 's/^/  /'
-echo "BLOCKY internal UNIFI custom query"
-dig +short @192.168.4.121 unifi.housefam.win | sed 's/^/  /'
+echo "UNIFI internal device query"
+dig +short @192.168.4.1 unifi.internal | sed 's/^/  /'
+echo "UNIFI internal custom query"
+dig +short @192.168.4.1 unifi.housefam.win | sed 's/^/  /'
 echo "---"
-echo "BLOCKY main cluster query"
-dig +short @192.168.4.121 echo-server.housefam.casa | sed 's/^/  /'
+echo "UniFi main cluster query"
+dig +short @192.168.4.1 echo-server.housefam.casa | sed 's/^/  /'
 echo "---"
-echo "BLOCKY reverse query"
-dig +short @192.168.4.121 -x 192.168.7.28 | sed 's/^/  /'
+echo "UniFi reverse query"
+dig +short @192.168.4.1 -x 192.168.7.28 | sed 's/^/  /'
 echo "---------------------------------------------------"
